185cda754d412602dda5421ded9c01315fed5978
humbertoguerrer <humberto.guerrer@gmail.com>
2021-05-23 21:40:32 -0300

    Model e Repository criados

---
+++ src/main/java/com/hgn/saladereuniao/controller/RoomController.java
@@ -0,0 +1,7 @@
+package com.hgn.saladereuniao.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RoomController {
+}
---
+++ src/main/java/com/hgn/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.hgn.saladereuniao.repository;
+
+import com.hgn.saladereuniao.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
