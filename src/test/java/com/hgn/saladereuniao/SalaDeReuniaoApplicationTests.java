3def9ded80cfea48055b416b2b4c65c680cde7e2
humbertoguerrer <humberto.guerrer@gmail.com>
2021-05-23 21:14:57 -0300

    Projeto criado

---
+++ src/test/java/com/hgn/saladereuniao/SalaDeReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgn.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaDeReuniaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
